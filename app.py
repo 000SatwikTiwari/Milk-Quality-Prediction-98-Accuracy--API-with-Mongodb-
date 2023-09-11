1aada165a1ae3f60ec0b1c0d2a22abe07f1880e1
Satwik Tiwari <144311322+000SatwikTiwari@users.noreply.github.com>
2023-09-11 11:44:05 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import sklearn
-import numpy as np
-import json
-import imblearn
-import pandas as pd
-from sklearn.metrics import mean_squared_error,r2_score,mean_absolute_error
-model=pickle.load(open('model.pkl','rb'))
